dfd014232cd2a175fb1815812c677b9359a95e12
thequbit <thequbit@gmail.com>
2013-08-10 22:23:22 -0400

    updated db creation script to include indexes and moved some columns around

--- src/scraper/db/createdb.sql
+++ src/scraper/db/createdb.sql
@@ -11,8 +11,28 @@ create table users(
 userid int not null auto_increment primary key,
 name varchar(256) not null,
 login varchar(256) not null,
-passhash varchar(256) not null
+passhash varchar(256) not null,
+lastlogin datetime
 );
+
+create index users_name on users(name);
+create index users_login on users(login);
+
+create table userinfo(
+userinfoid int not null,
+userid int not null,
+foreign key (userid) references users(userid),
+enabled bool not null,
+email varchar(256),
+twitter varchar(256),
+phone varchar(256),
+pushinterval int not null
+);
+
+create index userinfo_userid on userinfo(userid);
+create index userinfo_email on userinfo(email);
+create index userinfo_twitter on userinfo(twitter);
+create index userinfo_phone on userinfo(phone);
 
 create table urls(
 urlid int not null auto_increment primary key,
@@ -23,6 +43,9 @@ createdatetime datetime not null,
 creationuserid int not null,
 foreign key (creationuserid) references users(userid)
 );
+
+create index urls_name on urls(name);
+create index urls_creationuserid on urls(creationuserid);
 
 create table userurls(
 userurlid int not null auto_increment primary key,
@@ -35,12 +58,18 @@ notes text,
 adddatetime datetime not null
 );
 
+create index userurls_urlid on userurls(urlid);
+create index userurls_userid on userurls(userid);
+
 create table phrases(
 phraseid int not null auto_increment primary key,
 phrase varchar(256) not null,
 userid int not null,
 foreign key (userid) references users(userid)
 );
+
+create index phrases_phrase on phrases(phrase);
+create index phrases_userid on phrases(userid);
 
 create table urlphrases(
 urlphraseid int not null auto_increment primary key,
@@ -51,6 +80,10 @@ foreign key (urlid) references urls(urlid),
 phraseid int not null,
 foreign key (phraseid) references phrases(phraseid)
 );
+
+create index urlphrases_userid on urlphrases(userid);
+create index urlphrases_urlid on urlphrases(urlid);
+create index urlphrases_phraseid on urlphrases(phraseid);
 
 create table runs(
 runid int not null auto_increment primary key,
@@ -67,16 +100,21 @@ urlid int not null,
 foreign key (urlid) references urls(urlid)
 );
 
+create index scraps_urlid on scraps(urlid);
+
 create table docs(
 docid int not null auto_increment primary key,
+docurl text not null,
 filename text not null,
 linktext text not null,
 downloaddatetime datetime not null,
 doctext text not null,
-hash text not null,
+dochash text not null,
 urlid int not null,
 foreign key (urlid) references urls(urlid)
 );
+
+create index docs_urlid on docs(urlid);
 
 create table finds(
 findid int not null auto_increment primary key,
@@ -86,3 +124,8 @@ finddatetime datetime not null,
 docid int not null,
 foreign key (docid) references docs(docid)
 );
+
+create index finds_urlphraseid on finds(urlphraseid);
+create index finds_docid on finds(docid);
+
+

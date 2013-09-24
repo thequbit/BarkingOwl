a35dcd3035c6d8ea90cd84da68410f5d3b505d47
thequbit <thequbit@gmail.com>
2013-09-23 20:07:01 -0400

    moved to sqlalchemy ... significant rewrite

--- src/scraper/runscrapper.py
+++ src/scraper/runscrapper.py
@@ -1,19 +1,25 @@
-from db.urls import urls
-from db.phrases import phrases
-from db.docs import docs
-from db.urlphrases import urlphrases
-from db.finds import finds
-from db.scraps import scraps
-from db.runs import runs
+from sqlalchemy import *
+from sqlalchemy.orm import (
+    sessionmaker,
+    )
+from sqlalchemy.ext.declarative import declarative_base
 
 from scrapper import scrapper
 
 from time import strftime
 from time import clock
-
 import os
+import threading
 
-import pprint
+from models import *
+
+engine = create_engine('mysql://bouser:password123%%%@lisa.duffnet.local', echo=True)
+engine.execute("USE barkingowl")
+Session = sessionmaker(bind=engine)
+DBSession = Session()
+Base = declarative_base()
+Base.metadata.create_all(engine)
+
 
 verbose = True
 
@@ -21,82 +27,69 @@ def _report(text):
     if verbose:
         print "[Scrapper] {0}".format(text)
 
-def getcreds():
-    return ("lisa.duffnet.local","bouser","password123%%%","barkingowl")
-
 def geturls():
-    host,user,passwd,db = getcreds()
-    u = urls(host,user,passwd,db)
-    _urls = u.getall()
-    return _urls
+    urls = DBSession.query(UrlModel)
+    return urls
 
 def gethashs():
-    host,user,passwd,db = getcreds()
-    d = docs(host,user,passwd,db)
-    _hashs = d.gethashs()
-    return _hashs
+    hashs = DBSession.query(DocModel.dochash).all()
+    return hashs
 
-def addrun(start,end,success):
-    host,user,passwd,db = getcreds()
-    r = runs(host,user,passwd,db)
-    r.add(start,end,success)
+def addrun(start,end,success,urlid):
+    run = RunModel(start,end,success,urlid)
+    DBSession.add(run)
 
-def addscrape(start,end,success,urlid):
-    host,user,passwd,db = getcreds()
-    s = scraps(host,user,passwd,db)
-    s.add(start,success,urlid)
+def adddoc(docurl,filename,linktext,downloaded,created,doctext,dochash,urlid):
+    doc = DocModel(docurl,filename,linktext,downloaded,created,doctext,dochash,urlid)
+    DBSession.add(doc)
+    return doc.id
 
-def adddoc(docurl,filename,linktext,dldt,text,dochash,urlid):
-    host,user,passwd,db = getcreds()
-    d = docs(host,user,passwd,db)
-    docid = d.add(docurl,filename,linktext,dldt,text,dochash,urlid)
-    return docid
-
-def getphrases(urlid):
-    host,user,passwd,db = getcreds()
-    p = phrases(host,user,passwd,db)
-    _phrases = p.getbyurlid(urlid)
-    return _phrases
-
-def savefind(urlphraseid,dt,docid):
-    host,user,passwd,db = getcreds()
-    f = finds(host,user,passwd,db)
-    findid = f.add(urlphraseid,dt,docid)
-    return findid
-
-def runscraper(destinationdirectory,linklevel):
-    starttime = strftime("%Y-%m-%d %H:%M:%S")
-    _report("Started at: {0}".format(starttime))
+def scrapurls(urls,destinationdirectory,linklevel):
     retsuccess = True
-    _urls = geturls()
-    _report("Working on {0} URLs ...".format(len(_urls)))
-    for _url in _urls:
-        urlid,url,name,desc,dt,cuserid = _url
-        _report("Running on: {0}".format(url))
-        scrapestarttime = strftime("%Y-%m-%d %H:%M:%S")
-        docs,success = scrapper(url,destinationdirectory,linklevel)
-        scrapeendtime = strftime("%Y-%m-%d %H:%M:%S") 
-        addscrape(scrapestarttime,scrapeendtime,success,urlid)
+    #_report("Working on {0} URLs ...".format(len(urls)))
+    for url in urls:
+        starttime = strftime("%Y-%m-%d %H:%M:%S")
+        _report("Started at: {0}".format(starttime))
+        _report("Running on: {0}".format(url.url))
+        docs,success = scrapper(url.url,destinationdirectory,linklevel)
         if success:
             _report("Found {0} pdf documents.".format(len(docs)))
             for doc in docs:
                 hashs = gethashs()
-                url,filename,linktext,dldt,text,dochash,tokens = doc
+                docurl,filename,linktext,downloaded,created,doctext,dochash,tokens = doc
                 if not dochash in hashs:
                     _report("Adding doc: {0} [{1}]".format(filename,dochash))
-                    docid = adddoc(url,filename,linktext,dldt,text,dochash,urlid)
-                    phrases = getphrases(urlid)
-                    for p in phrases:
-                        phrase,userid,urlphraseid = p
-                        if phrase in text:
-                            now = strftime("%Y-%m-%d %H:%M:%S")
-                            savefind(urlphraseid,now,docid)
-                        # TODO: send notification to user
+                    docid = adddoc(docurl,filename,linktext,downloaded,created,doctext,dochash,url.id)
+                    #
+                    #   NOTE: Removing this section for first rev of site
+                    #
+                    #phrases = getphrases(urlid)
+                    #for p in phrases:
+                    #    phrase,userid,urlphraseid = p
+                    #    if phrase in text:
+                    #        now = strftime("%Y-%m-%d %H:%M:%S")
+                    #        savefind(urlphraseid,now,docid)
+                    #    # TODO: send notification to user
                 else:
                     _report("Skipping doc, already processed.")
-    endtime = strftime("%Y-%m-%d %H:%M:%S")
-    addrun(starttime,endtime,retsuccess)
+        endtime = strftime("%Y-%m-%d %H:%M:%S")
+        addrun(starttime,endtime,retsuccess,url.id)
     return retsuccess
+
+def split_list(alist, wanted_parts=1):
+    length = len(alist)
+    return [ alist[i*length // wanted_parts: (i+1)*length // wanted_parts]
+             for i in range(wanted_parts) ]
+
+def runscraper(destinationdirectory,linklevel):
+    allurls = []
+    for url in geturls():
+        allurls.append(url)
+    urllist = split_list(allurls)
+    for urls in urllist:
+        print type(urllist)
+        t = threading.Thread(target=scrapurls, args=(urls,destinationdirectory,linklevel))
+        t.start()
 
 def deletefiles(folder):
     for the_file in os.listdir(folder):
@@ -108,17 +101,17 @@ def deletefiles(folder):
             pass
 
 # Taken from http://stackoverflow.com/users/165216/paul-mcguire
-#
 # http://stackoverflow.com/questions/1557571/how-to-get-time-of-a-python-program-execution
-#
 def secondsToStr(t):
     return "%d:%02d:%02d.%03d" % \
         reduce(lambda ll,b : divmod(ll[0],b) + ll[1:],
             [(t*1000,),1000,60,60])
 
 def main():
+    linklevel = 5
+    _report("Running scraper with a link depth of {0} ...".format(linklevel))
     starttime = clock()
-    runscraper('./downloads',3)
+    runscraper('./downloads',linklevel)
     _report("Cleaning up downloaded files.")
     deletefiles('./downloads')
     endtime = clock()

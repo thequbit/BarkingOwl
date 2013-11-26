09ba9f43b52993dc78ec0c1068226172c8a50856
thequbit <thequbit@gmail.com>
2013-11-26 09:39:34 -0500

    removed old version

--- src/db/README.md
+++
@@ -1,3 +0,0 @@
-## install ##
-
-todo: this.
--- src/scraper/models/__dbcreds__.py
+++
@@ -1,7 +0,0 @@
-
-### Configurion your SQL connection settings here ###
-
-__username__ = ''
-__password__ = ''
-__database__ = 'barkingowl'
-__server__   = ''

a482cea40cd43fa51904e1cc7c91edc70349bc7d
Mihir Singh <me@mihirsingh.com>
2014-02-21 21:35:42 -0500

    Removed controller from BarkingOwl

--- controller/models/__dbcreds__.py
+++
@@ -1,7 +0,0 @@
-
-### Configurion your SQL connection settings here ###
-
-__username__ = 'bouser'
-__password__ = 'password123%%%'
-__database__ = 'bodispatcherdb'
-__server__   = 'lisa.duffnet.local'

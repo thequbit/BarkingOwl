97237ca7f7e4f4865d832ae75d22d315e9b73d42
thequbit <thequbit@gmail.com>
2014-02-22 00:45:08 -0500

    added description and clasifiers

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
 from setuptools import setup
 
 long_description = \
-'''YADA
+'''
 
-YADA
+BarkingOwl is a scalable web crawler intended to be used to
+find specific document types such as PDFs.
+
 '''
 
 setup(
-    name="Barking Owl",
+    name="BarkingOwl",
     version="0.0.1",
     license="GPL2",
     author="Timothy Duffy",
     author_email="tim@timduffy.me",
     packages=["barking_owl"],
     zip_safe=False,
-    description='YADA YADA YADA',
+    description='Scalable web crawler.',
     long_description=long_description,
     include_package_data=True,
     platforms="any",
@@ -29,5 +31,11 @@ setup(
       "wsgiref==0.1.2",
     ], 
     url="https://github.com/thequbit/BarkingOwl",
-    classifiers=[],
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Environment :: Console',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+      'Operating System :: POSIX',
+    ],
 )

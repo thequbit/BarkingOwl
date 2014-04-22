ca0b0edcccfebfc52051b568daef3c32089fda17
thequbit <thequbit@gmail.com>
2014-04-21 22:30:23 -0400

    updated some issues with debug throwing an error, and reduced the amount of debug from scraper

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ TXT, HTML, etc.
 
 setup(
     name="BarkingOwl",
-    version="0.0.16",
+    version="0.0.17",
     license="GPL3",
     author="Timothy Duffy",
     author_email="tim@timduffy.me",

24ba7dcbaa94e1d76496c175dc02c753ed7378c9
thequbit <thequbit@gmail.com>
2015-01-03 17:04:33 -0500

    added URLParameters to barking_owl package so pika does not need to be imported

--- barking_owl/__init__.py
+++ barking_owl/__init__.py
@@ -2,3 +2,4 @@ from .scraper import Scraper,ScraperWrapper
 from .dispatcher import Dispatcher
 from .globalshutdown import GlobalShutdown
 from .busaccess import BusAccess
+from pika import URLParameters as URLParameters
